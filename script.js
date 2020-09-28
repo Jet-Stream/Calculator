106b87b2a42d0b3ad8ca5ae539397fad5b3472da
Tanner <tjohnsto8@gmail.com>
2020-09-28 11:14:23 -0400

    Began attaching event listeners to button divs

--- script.js
+++ script.js
@@ -28,7 +28,23 @@ const calc = function operate(x,y,z){
         return quotient(x,z);
     }
 }
-let numberButtons = document.getElementsByClassName('numberButton')
-  numberButtons.addEventListener('click', () =>{
-    //add clicked button to field box
-  });
+//add event listener to first number button that adds '1' to field box
+const numberButtons = Array.from(document.getElementsByClassName('numberButton'));
+console.log(numberButtons[0]);
+let firstNumberBox = document.getElementById('calcDiv1');
+for(let i=0; i<= numberButtons.length; i++){
+numberButtons[i].addEventListener('click', () =>{
+    firstNumberBox.innerHTML += numberButtons[i].firstChild.textContent;
+});
+}
+//if calcDiv1, operatorDiv and calcDiv2 are all blank, clicked numbers go to calcDiv1
+//if calcDiv1 and operatorDiv have content, clicked numbers go to calcDiv2
+const operatorButtons = Array.from(document.getElementsByClassName('operatorButton'));
+console.log(operatorButtons[0]);
+let operatorBox = document.getElementById('operatorDiv');
+for(let i=0; i<= operatorButtons.length; i++){
+    operatorButtons[i].addEventListener('click', () => {
+         operatorBox.innerHTML += operatorButtons[i].firstChild.textContent;
+     });
+ }
+
