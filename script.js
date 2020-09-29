a6078d8164ae7ec97b4164d3d0433e8629f6dcca
Tanner <tjohnsto8@gmail.com>
2020-09-28 21:47:34 -0400

    Added calculation functionality to event listeners, need to update logic to handle chained calculations

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
-const sum = function add(x,y){
-    return x + y;
-}
-const difference = function subtract(x,y){
-    return x - y;
-}
-const product = function multiply(x,y){
-    return x * y;
-}
-const quotient = function divide(x,y){
-    if(x / y == Infinity){
-        return 'no dividing by zero!'}
-    else {
-        return x/y;
-        }
-}
-const calc = function operate(x,y,z){
+const calculate = function (x,y,z){
     if(y == '+'){
-        return sum(x,z);
+        return x+z;
     }
     if(y == '-'){
-        return difference(x,z);
+        return x-z;
     }
     if(y == '*'){
-        return product(x,z);
+        return x*z;
     }
     if(y == '/'){
-        return quotient(x,z);
+        return x/z;
     }
 }
-//add event listener to first number button that adds '1' to field box
-const numberButtons = Array.from(document.getElementsByClassName('numberButton'));
-console.log(numberButtons[0]);
-let firstNumberBox = document.getElementById('calcDiv1');
-for(let i=0; i<= numberButtons.length; i++){
-numberButtons[i].addEventListener('click', () =>{
-    firstNumberBox.innerHTML += numberButtons[i].firstChild.textContent;
-});
-}
+const clear = function(number1, operator, number2){
+    firstNumberBox.innerHTML = '';
+    operatorBox.innerHTML = '';
+    secondNumberBox.innerHTML = '';
+    resultBox.innerHTML = '';
+    result = undefined;
+};
 //if calcDiv1, operatorDiv and calcDiv2 are all blank, clicked numbers go to calcDiv1
-//if calcDiv1 and operatorDiv have content, clicked numbers go to calcDiv2
-const operatorButtons = Array.from(document.getElementsByClassName('operatorButton'));
-console.log(operatorButtons[0]);
-let operatorBox = document.getElementById('operatorDiv');
-for(let i=0; i<= operatorButtons.length; i++){
-    operatorButtons[i].addEventListener('click', () => {
-         operatorBox.innerHTML += operatorButtons[i].firstChild.textContent;
-     });
- }
+//if operatorDiv has content, clicked numbers go to calcDiv2
 
+let numberButtons = Array.from(document.getElementsByClassName('numberButton'));
+let firstNumberBox = document.getElementById('calcDiv1');
+let operatorBox = document.getElementById('operatorDiv');
+let secondNumberBox = document.getElementById('calcDiv2');
+let resultBox = document.getElementById('resultDiv');
+let operatorButtons = Array.from(document.getElementsByClassName('operatorButton'));
+let total;
+
+//for each item in numberButtonsArray, attach event listener
+numberButtons.forEach(element =>  
+    element.addEventListener('click', () => {
+         
+        if(total != undefined){
+            secondNumberBox.innerHTML = element.firstChild.textContent;
+        }
+        else if(operatorBox.innerHTML != ''){
+        secondNumberBox.innerHTML += element.firstChild.textContent;
+    }
+        else {
+            firstNumberBox.innerHTML += element.firstChild.textContent;
+        }
+}));
+
+operatorButtons.forEach(element =>  
+    element.addEventListener('click', () => {
+
+        if(element.id === 'clear'){
+            clear(firstNumberBox,operatorBox,secondNumberBox,resultBox);
+            } 
+        else if(total != undefined){
+                firstNumberBox.innerHTML = total;
+                operatorBox.innerHTML = element.firstChild.textContent;
+            }
+  
+        else if(element.id === 'equals'){
+        total = resultBox.innerHTML = calculate(parseInt(firstNumberBox.innerHTML),operatorBox.innerHTML,parseInt(secondNumberBox.innerHTML));
+        console.log(total);
+        }
+        else if(operatorBox != ''){
+        operatorBox.innerHTML = element.firstChild.textContent;
+        }
+        else{
+        operatorBox.innerHTML += element.firstChild.textContent;
+        }
+}));
