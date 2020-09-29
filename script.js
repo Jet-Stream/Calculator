e0d19359e87f18691041b5ee222279b3c48b28a3
Tanner <tjohnsto8@gmail.com>
2020-09-28 23:36:35 -0400

    chained calculation logic implemented, equal sign button logic still needed

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const calculate = function (x,y,z){
         return x/z;
     }
 }
-const clear = function(number1, operator, number2){
+const clear = function(){
     firstNumberBox.innerHTML = '';
     operatorBox.innerHTML = '';
     secondNumberBox.innerHTML = '';
     resultBox.innerHTML = '';
-    result = undefined;
+    total = undefined;
 };
 //if calcDiv1, operatorDiv and calcDiv2 are all blank, clicked numbers go to calcDiv1
 //if operatorDiv has content, clicked numbers go to calcDiv2
@@ -35,7 +35,7 @@ numberButtons.forEach(element =>
     element.addEventListener('click', () => {
          
         if(total != undefined){
-            secondNumberBox.innerHTML = element.firstChild.textContent;
+            secondNumberBox.innerHTML += element.firstChild.textContent;
         }
         else if(operatorBox.innerHTML != ''){
         secondNumberBox.innerHTML += element.firstChild.textContent;
@@ -49,20 +49,26 @@ operatorButtons.forEach(element =>
     element.addEventListener('click', () => {
 
         if(element.id === 'clear'){
-            clear(firstNumberBox,operatorBox,secondNumberBox,resultBox);
-            } 
+            clear();
+            }
+        else if(element.id === 'equals'){
+                if(firstNumberBox.innerHTML === '' && operatorBox.innerHTML === '' && secondNumberBox.innerHTML === ''){
+                    resultBox = '';
+                    }
+                else{
+                    total = resultBox.innerHTML = calculate(parseInt(firstNumberBox.innerHTML),operatorBox.innerHTML,parseInt(secondNumberBox.innerHTML));
+                    secondNumberBox.innerHTML = '';    
+                    }
+        }
         else if(total != undefined){
                 firstNumberBox.innerHTML = total;
                 operatorBox.innerHTML = element.firstChild.textContent;
             }
   
-        else if(element.id === 'equals'){
-        total = resultBox.innerHTML = calculate(parseInt(firstNumberBox.innerHTML),operatorBox.innerHTML,parseInt(secondNumberBox.innerHTML));
-        console.log(total);
-        }
         else if(operatorBox != ''){
         operatorBox.innerHTML = element.firstChild.textContent;
         }
+
         else{
         operatorBox.innerHTML += element.firstChild.textContent;
         }
